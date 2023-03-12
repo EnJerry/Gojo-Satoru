f31592daf49464b653538d6323a206496407093d
Gabrielgabb <126182247+Gabrielgabb@users.noreply.github.com>
2023-03-12 13:46:29 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,7 +15,7 @@ global.APIs =
 
 //Api Key\\
 global.APIKeys = {
-	'https://zenzapis.xyz': 'sanzychan01', //f7dae38ce06a\\
+	'https://zenzapis.xyz': 'sanzychan01', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
 }
 
 //customize settings\\
